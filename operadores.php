2d6ede1d5bddf1835b9c5d92f72edd9dd493c8b4
FrancoFabian76 <francancela2017@gmail.com>
2025-04-03 14:12:20 -0300

    Uso del "*="

--- operadores.php
+++ operadores.php
@@ -3,5 +3,6 @@ $edad = 1;
 $edad+= 1;
 echo "$edad"; 
 
-/* El uso de "+=" es para sumar un numero y guardarlo. Tambien el "-=" es para restar un numero y guardarlo. */
+/* El uso de "+=" es para sumar un numero y guardarlo. Tambien el "-=" es para restar un numero y guardarlo. 
+El uso de "*=" es para multiplicar y guarda*/
 ?>
