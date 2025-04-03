ee9c241aaf6996985e5a182170b3426871737e2c
FrancoFabian76 <francancela2017@gmail.com>
2025-04-03 14:04:18 -0300

    Prueba del uso de comillas y concatenar.

---
+++ comillas.php
@@ -0,0 +1,9 @@
+<?php 
+
+$emisora = 'Radio Disney';
+echo "Me gusta escuchar $emisora";
+echo 'Me gusta escuchar $emisora';
+/* Las comillas simples (') no permiten el uso de las variables a diferencia de
+las comillas dobles (") que si te permiten el uso de variables. */
+
+?>
---
+++ concatenar.php
@@ -0,0 +1,6 @@
+<?php 
+$nombre = "Franco";
+$saludo = "Hola ";
+echo "$nombre.$saludo";
+
+?>
