107d7f5145adaa8893d72f0cfcc3d0eb8e3e2c27
FrancoFabian76 <francancela2017@gmail.com>
2025-04-03 14:20:59 -0300

    Operadores

--- operadores.php
+++ operadores.php
@@ -3,6 +3,17 @@ $edad = 1;
 $edad+= 1;
 echo "$edad"; 
 
-/* El uso de "+=" es para sumar un numero y guardarlo. Tambien el "-=" es para restar un numero y guardarlo. 
-El uso de "*=" es para multiplicar y guarda*/
+/* 
+- El uso de "+=" es para sumar un numero y guardarlo. 
+- El uso de "-=" es para restar un numero y guardarlo. 
+- El uso de "*=" es para multiplicar y guardarlo.
+- El uso de "/=" es para dividir y asignar.
+- El uso de "%=" es para  modulo y asignar.
+*/
+$saludo = "Hola";
+$saludo .= "Franco"
+
+/*
+- El uso de ".=" es para concatenar y asignar.
+*/
 ?>
