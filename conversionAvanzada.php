418fc3e5c02f1e91c7d9d54ebc127eb9a490b8f8
FrancoFabian76 <francancela2017@gmail.com>
2025-05-15 14:58:30 -0300

    ConversionAvanzada "terminada"

--- conversionAvanzada.php
+++ conversionAvanzada.php
@@ -1,3 +1,28 @@
 <?php 
+if ($_SERVER ["REQUEST_METHOD"] == "POST") {
+    $cantidadPesos = floatval( $_POST["cantidad"]);
+    $valorDolar = 40;
+    $valorEuro = 43;
+    $valorReal = 8;
+    $moneda = $_POST["moneda"];
+    switch ($moneda) {
+        case "USD":
+        $cantidadFinal = $cantidadPesos / $valorDolar; 
+        echo "La canttidad es: $cantidadFinal"; 
+        break;
+        case "EUR":
+        $cantidadFinal = $cantidadPesos / $valorEuro;
+        echo "La cantidad es: $cantidadFinal";
+        break;
+        case "BRL":
+        $cantidadFinal = $cantidadPesos / $valorReal;
+        echo "La cantidad es: $cantidadFinal";
+        break;
+        default;
+        echo "Error en la solicitud.";
+    }
 
+    }else{
+        echo "error";
+    }
 ?>
