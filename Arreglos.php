e4899c57a18c0b32ad559b79ce233223ab245b5b
FrancoFabian76 <francancela2017@gmail.com>
2025-05-15 15:39:06 -0300

    Arreglos.

---
+++ Arreglos.php
@@ -0,0 +1,5 @@
+<?php 
+$semana = ['Lunes']; //Esto es un arreglo con un elemento llamado "Lunes" en el espacio 0
+$semana[]= 'Martes'; /* Esto agrega "Martes" al arreglo. Usando [] sin un numero agrega al ultimo espacio disponible. 
+En caso de colocar un numero, por ejemplo [0] este remplaza "Lunes" y si es [1] remplaza el espacio 1.*/
+?>
--- conversionAvanzada.php
+++ conversionAvanzada.php
@@ -1,21 +1,25 @@
 <?php 
+    //$valorDolar = 40;
+    //$valorEuro = 43;
+    //$valorReal = 8;
+
+    $valorMoneda = [40, 43, 8, "Hola"];
+    var_dump( $valorMoneda );
+
 if ($_SERVER ["REQUEST_METHOD"] == "POST") {
     $cantidadPesos = floatval( $_POST["cantidad"]);
-    $valorDolar = 40;
-    $valorEuro = 43;
-    $valorReal = 8;
     $moneda = $_POST["moneda"];
     switch ($moneda) {
         case "USD":
-        $cantidadFinal = $cantidadPesos / $valorDolar; 
+        $cantidadFinal = $cantidadPesos / $valorMoneda[0]; 
         echo "La canttidad es: $cantidadFinal"; 
         break;
         case "EUR":
-        $cantidadFinal = $cantidadPesos / $valorEuro;
+        $cantidadFinal = $cantidadPesos / $valorMoneda[1];
         echo "La cantidad es: $cantidadFinal";
         break;
         case "BRL":
-        $cantidadFinal = $cantidadPesos / $valorReal;
+        $cantidadFinal = $cantidadPesos / $valorMoneda[2];
         echo "La cantidad es: $cantidadFinal";
         break;
         default;
