a7180fa46b35143be97dc343708fb9fa9a29e695
FrancoFabian76 <francancela2017@gmail.com>
2025-05-22 15:35:19 -0300

    Actividad 1 array

---
+++ Actividad1Arreglos.php
@@ -0,0 +1,21 @@
+<?php 
+
+$tripulacion=[
+"Alex Rider",
+"Mia Kovalik",
+"Raj Patel",
+"Elena Vega",
+"Jules Bergman" 
+];
+$tripulacion[1]= "Nadia Petrov";
+$tripulacion[]= "Sam Carter";
+$tripulacion[]= "Leo Wu";
+$tripulacion[]= "Zara Khan";
+
+foreach($tripulacion as $tripulantes){
+    echo($tripulantes);
+
+}
+
+
+?>
