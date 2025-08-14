c9af9b157fc5b09db526f9ce68b2289b941d93f9
FrancoFabian76 <francancela2017@gmail.com>
2025-08-14 15:35:29 -0300

    Inicio de JavaScript

---
+++ javascript/coso2.js
@@ -0,0 +1,8 @@
+var nombre = "Ana"; // Ambito de funcion (variable global) (evitar)
+let edad = 25; // Ambito de bloque (variable local) (recomendado)
+const PI = 3.1316; // Constante (inmutable)
+
+function calcularCirculo(radio){
+
+    
+}
