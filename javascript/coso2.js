6d1f9053621b02526719e6c5a4b2c41ee8e0bd5f
FrancoFabian76 <francancela2017@gmail.com>
2025-08-28 15:37:25 -0300

    Practicando con Java Script y la consola

--- javascript/coso2.js
+++ javascript/coso2.js
@@ -2,7 +2,23 @@ var nombre = "Ana"; // Ambito de funcion (variable global) (evitar)
 let edad = 25; // Ambito de bloque (variable local) (recomendado)
 const PI = 3.1316; // Constante (inmutable)
 
+
+let radio = prompt("ingrese el radio del circulo: ");
+let radioEnNumero = parseInt(radio);
+
 function calcularCirculo(radio){
 
-    
+    let resultado = (radio **2) / Math.PI
+    return resultado
 }
+
+let ancho = prompt("¿Cuanto mide el Ancho?: ");
+let largo = prompt("¿Cuanto mide el Largo?: ");
+let anchoEnNumero = parseInt(ancho);
+let largoEnNumero = parseInt(largo);
+
+function calcularAreaRectangulo(alto, ancho){
+return ancho * largo;
+}
+
+
