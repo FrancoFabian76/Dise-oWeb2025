b976ab0658cfa4a6273f254ab4ed0645efbfced7
FrancoFabian76 <francancela2017@gmail.com>
2025-10-09 15:40:05 -0300

    tomto

--- Actividad Post/POST.js
+++ Actividad Post/POST.js
@@ -1,15 +1,41 @@
-fetch('api/registro.php',{
-    method: 'post',
-    headers: {
-        'content-type': 'application/json'
-    },
-    body: JSON.stringify({
-        nombre: 'nombre',
-        correo: 'correo@gmail.com'
-    })
-})
-.then(response => response.JSON())
-.then(data => {
-    console.log('respuesta del servidor:', data);
-})
-.catch(error => console.log('error:', error));
+window.addEventListener("DOMContentLoaded", () => {
+
+
+    const boton = document.getElementById('btn');
+   
+    
+    boton.addEventListener("click" , (e) => 
+        {
+            e.preventDefault();
+
+
+            const nombre = document.getElementById('nombre').value;
+            const correo = document.getElementById('correo').value;
+            
+    
+    
+            fetch('api/registro.php',{
+                method: 'post',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    nombre: nombre,
+                    correo: correo
+                })
+            })
+            .then(response => response.JSON())
+            .then(data => {
+                console.log('respuesta del servidor:', data);
+            })
+            .catch(error => console.log('error:', error));
+    
+        });
+    
+    
+    
+
+
+});
+
+
