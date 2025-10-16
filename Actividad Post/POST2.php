e627ef8732ef839778b90d34bb495af894eeabff
FrancoFabian76 <francancela2017@gmail.com>
2025-10-16 14:25:18 -0300

    .

--- Actividad Post/POST2.php
+++ Actividad Post/POST2.php
@@ -5,6 +5,10 @@ header('Access-Control-Allow-Methods: POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 
 $input = json_decode(file_get_contents("php://input"), true);
+if (isset($input[""]) && isset($input[""][  ""]) && isset($input[""][""])) {    
+    echo json_encode(["estado" => "error", "mensaje" => "No se permiten campos vacios."]);
+    exit;
+}
 
 if (!$input['nombre'] || !$input['razon'] || !$input['correo'] || !$input['telefono'] || !$input['producto']){
     echo json_encode(["estado" => "error", "mensaje" => "Faltan campos obligatorios."]);
